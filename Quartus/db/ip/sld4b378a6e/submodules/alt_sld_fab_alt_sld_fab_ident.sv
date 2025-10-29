9e344420960e16c0fb438a197364db6be38e8031
unknown <noah.white@ufl.edu>
2025-10-28 23:05:25 -0400

    Finished! YAYYYYY!!! I finished testing the branch instructions in MyMIF.mif. I also used the provided files to test as well but I did have to change them a little bit because of how I do addressing slightly different

--- Quartus/db/ip/sld4b378a6e/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ Quartus/db/ip/sld4b378a6e/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 22616eb70004761651f7
+//   DESIGN_HASH   : 3128421426815fbfa4e7
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h22616eb70004761651f7 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h3128421426815fbfa4e7 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
